06fa05ff66280fb27c1944596e59d160d4658754
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-05-27 14:02:04 +0800

    [feat] update function of viewmodel.

--- app/src/main/java/com/example/kotlinroomdatabase/viewmodel/TodoVIewModel.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/viewmodel/TodoVIewModel.kt
@@ -1,6 +1,20 @@
 package com.example.kotlinroomdatabase.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.kotlinroomdatabase.model.Todo
 
-class TodoVIewModel: ViewModel() {
+class TodoVIewModel : ViewModel() {
+    private val _todoList = MutableLiveData<List<Todo>>()
+    var todoList: LiveData<List<Todo>> = _todoList
+
+    fun getAllTodo() {
+    }
+
+    fun addTodo() {
+    }
+
+    fun deleteTodo() {
+    }
 }
