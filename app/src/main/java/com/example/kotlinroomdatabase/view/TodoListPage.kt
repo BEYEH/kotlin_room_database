74e4f44d467cea9933139e30cbc2846c277e4837
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-05-27 10:42:22 +0800

    [feat] update ui - use column.

--- app/src/main/java/com/example/kotlinroomdatabase/view/TodoListPage.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/view/TodoListPage.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinroomdatabase.view
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,8 +11,9 @@ import com.example.kotlinroomdatabase.data.getFakeTodo
 fun TodoListPage(modifier: Modifier) {
     val todoList = getFakeTodo()
 
-    Text(
-        text = todoList.toString(),
-        modifier = modifier
-    )
+    Column(modifier = modifier) {
+        Text(
+            text = todoList.toString()
+        )
+    }
 }
