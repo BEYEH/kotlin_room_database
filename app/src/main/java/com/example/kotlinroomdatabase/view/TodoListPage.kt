1a141c4cde62f8191a85e66de98c561d8320ab54
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-05-27 11:18:18 +0800

    [feat] update view - update the background of list.

--- app/src/main/java/com/example/kotlinroomdatabase/view/TodoListPage.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/view/TodoListPage.kt
@@ -1,13 +1,17 @@
 package com.example.kotlinroomdatabase.view
 
-import android.R.attr.text
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.kotlinroomdatabase.data.Todo
 import com.example.kotlinroomdatabase.data.getFakeTodo
 import java.text.SimpleDateFormat
@@ -31,7 +35,11 @@ fun TodoListPage(modifier: Modifier) {
 
 @Composable
 fun TodoItem(item: Todo) {
-    Row {
+    Row(
+        modifier = Modifier.fillMaxSize()
+            .padding(8.dp)
+            .background(MaterialTheme.colorScheme.primary)
+    ) {
         Column {
             Text(
                 text = SimpleDateFormat(
@@ -39,6 +47,7 @@ fun TodoItem(item: Todo) {
                     Locale.ENGLISH
                 ).format(item.createAt)
             )
+            Text(text = item.title)
         }
     }
 }
