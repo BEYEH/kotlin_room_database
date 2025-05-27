24932f5264515ad350bf6b98c0a6828f39279544
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-05-27 10:47:07 +0800

    [feat] update ui - use LazyColumn.

--- app/src/main/java/com/example/kotlinroomdatabase/view/TodoListPage.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/view/TodoListPage.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinroomdatabase.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,8 +14,12 @@ fun TodoListPage(modifier: Modifier) {
     val todoList = getFakeTodo()
 
     Column(modifier = modifier) {
-        Text(
-            text = todoList.toString()
+        LazyColumn(
+            content = {
+                itemsIndexed(todoList) { index: Int, item ->
+                    Text(text = item.toString())
+                }
+            }
         )
     }
 }
