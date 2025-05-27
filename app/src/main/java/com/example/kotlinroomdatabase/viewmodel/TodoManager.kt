72a2833df697368a4c457addc8113f4e779e25a2
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-05-27 14:13:21 +0800

    [feat] update function of manager.

--- app/src/main/java/com/example/kotlinroomdatabase/viewmodel/TodoManager.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/viewmodel/TodoManager.kt
@@ -1,4 +1,27 @@
 package com.example.kotlinroomdatabase.viewmodel
 
+import com.example.kotlinroomdatabase.model.Todo
+import java.time.Instant
+import java.util.Date
+
 object TodoManager {
+    private val todoList = mutableListOf<Todo>()
+
+    fun getAllTodo(): List<Todo> {
+        return todoList
+    }
+
+    fun addTodo(title: String) {
+        todoList.add(
+            Todo(
+                id = System.currentTimeMillis().toInt(),
+                title = title,
+                createAt = Date.from(Instant.now())
+            )
+        )
+    }
+
+    fun deleteTodo(id: Int) {
+        todoList.removeIf { it.id == id }
+    }
 }
